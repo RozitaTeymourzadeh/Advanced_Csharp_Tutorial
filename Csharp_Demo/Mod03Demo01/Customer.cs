abfd402536cdad5fc954bf63922671d7c57390c6
Rozita Teymourzadeh <Rozita.Teymourzadehneonode.com>
2017-01-30 12:54:31 +0100

    Module 3
    Construction, Struct

---
+++ Csharp_Demo/Mod03Demo01/Customer.cs
@@ -0,0 +1,48 @@
+﻿namespace Mod03Demo01
+{
+    public struct Customer
+    {
+        private string _name;// Field (backing variable for Name Property)
+        private string _email;
+
+        public int ID { get; set; }//created using "prob+tab+tab"
+        public CustomerStatusEnum Status { get; set; }
+        public string Name // Property
+        {
+            get
+            {
+                return _name;
+            }
+
+            set
+            {
+                //validation
+                _name = value;
+            }
+        }
+        public string Email // can be built using Encapsulate way. right click on _email Select "Refactor", Selec Encapsulate Field from _email to Email
+        {
+            get
+            {
+                return _email;
+            }
+
+            set
+            {
+                //validation
+                _email = value;
+            }
+        }
+
+        public string Details()
+        {
+            return string.Format("{0}\n{1}\n{2}\n{3}", ID,Name,Status,Email);
+        }
+    }
+    public enum CustomerStatusEnum
+    {
+        Silver,
+        Gold,
+        Platinum
+    }
+}
---
+++ Csharp_Demo/Mod03Demo01/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mod03Demo01 // Suould be the same Name with Customer namespace, so it can be used in this file. 
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int nextID = 1;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void CustomerList_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Customer current = (Customer)CustomerList.SelectedItem;
+            details.Text= current.Details();
+        }
+        private void Struct_Click_1(object sender, RoutedEventArgs e)
+        {
+            Customer cust = new Customer();// needs to be changed from "Customer cust;" to "Customer cust = new Customer();" because Name from struct Field concerted to Struct 
+            cust.ID = nextID++;
+            cust.Name = customerName.Text;
+            cust.Status = CustomerStatusEnum.Gold;
+            cust.Email = cust.Name + "@cust.com";
+            CustomerList.Items.Add(cust);
+        }
+
+
+    }
+}
