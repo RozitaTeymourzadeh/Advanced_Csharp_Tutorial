23dd0e67b7419fc74850c16b10c82d85af8610a0
Rozita Teymourzadeh <Rozita.Teymourzadehneonode.com>
2017-03-25 18:51:51 +0100

    Module 8: Accessing Data across the web

---
+++ Csharp_Demo/Mod08Demo01/Customer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mod08Demo01
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public CustomerStatusEnum Status { get; set; }
+        public string Email { get; set; }
+        public string Details()
+        {
+            return string.Format("Customer {0}\n{1}\n{2}\n{3}", ID, Name, Status,Email);
+        }
+    }
+    public enum CustomerStatusEnum
+    {
+        Gold,
+        Silver,
+        Platinum
+    }
+}
